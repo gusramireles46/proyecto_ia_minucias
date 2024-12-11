cbba2f9402fabbecd85fc0e621a659c9d9efd4ef
Gustavo Ramirez Mireles <gusramireles@hotmail.com>
2024-12-11 16:02:37 -0600

    Actualizacion de configuracion

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from Reconocedor import Reconocedor
+from config import DB_HOST, DB_USER, DB_PASSWORD, DB_NAME
 
 if __name__ == "__main__":
-    reconocedor = Reconocedor("localhost", "reconocedor", "@admin", "reconocedor")
+    reconocedor = Reconocedor(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
     archivos = ["letra_m", "letra_a", "letra_s", "letra_e", "letra_r", "letra_o"]
 
     # for archivo in archivos:
-    #     letras_matriz = reconocedor.entrenar(archivo + ".txt")
+    #     letras_matriz = reconocedor.entrenar("archivos_entrenamiento/" + archivo + ".txt")
     #     for letra, matriz in letras_matriz:
     #         reconocedor.calcular_frecuencias(letra, matriz)
 
